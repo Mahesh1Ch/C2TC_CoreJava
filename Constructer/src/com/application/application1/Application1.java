1c8b6354908a5bc11e7e52672c5efd92d2b970de
Mahesh1Ch <maheswarreddychagam555@gmail.com>
2023-02-15 17:10:58 +0530

    Commit

---
+++ Constructer/src/com/application/application1/Application1.java
@@ -0,0 +1,10 @@
+package com.application.application1;
+import java.util.ArrayList;
+
+public class Application1 {
+	public static void main(String args[]) {
+		System.out.println("AAA");
+		System.out.println("AAA");
+		
+	}
+}
---
+++ Constructer/src/com/application/application1/Constructer.java
@@ -0,0 +1,7 @@
+package com.application.application1;
+
+public class Constructer {
+	public static void main(String args[]) {
+	
+	}
+}
